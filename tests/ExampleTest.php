893666aa718f0b7b15e9bf3b6ced5101bdf2e51a
pkohrgra <pkohrgra@users.noreply.github.com>
2022-06-24 11:27:59 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,11 @@
 
 namespace Spatie\Weight\Tests;
 
-class ExampleTest extends TestCase{
-
+class ExampleTest extends TestCase
+{
     /** @test */
-    public function it_can_some_test(){
+    public function it_can_some_test()
+    {
         $this->assertTrue(true);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,6 @@
 
 namespace Spatie\Weight\Tests;
 
-abstract class TestCase extends \PHPUnit\Framework\TestCase{
-    
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
 }
